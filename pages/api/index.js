9f698e67861e3d01d2e3ece445ecefb24467791c
42Null <averyparanoiduser@gmail.com>
2024-01-04 14:30:44 -0600

    Fix index.js to snake remote host style differentiation.

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,12 +2,13 @@ export default function handler(req, res) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
 
+  //Not connected anymore to git tracking in order to diffrenteriate local & published copy in testing.
   const snakeInfo = {
     apiversion: "1",
     author: "42null",
-    color: "#2e77ff",//Color for deployed remote version, controlled via gitignore
-    head: "nr-rocket",
-    tail: "bolt",
+    color: "#000000",
+    head: "default",
+    tail: "default",
   };
 
   res.json(snakeInfo);
